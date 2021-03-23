aac0d15fc143a383598071114c9f5dc402dfe430
Gimena27 <66136801+Gimena27@users.noreply.github.com>
2021-03-23 12:15:31 -0500

    nuevas imagenes, html unificado y estilo

--- src/main.js
+++ src/main.js
@@ -1,6 +1,32 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
+import pokemon from './data/pokemon/pokemon.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+
+console.log(data);
+
+
+for (let i=0; i<data.pokemon.length; i++){
+    console.log(data.pokemon[i].type[0]);
+};
+
+
+const typePokemon = document.getElementById('pokeType');
+const mesage = document.createElement("span");
+
+
+typePokemon.addEventListener('click',function(){
+const newImg = document.getElementById('imagen');
+newImg.src= "../images/203876.jpg";
+});
+
+typePokemon.addEventListener('mouseover',function(){
+      mesage.innerHTML = "Aqui podras consultar los tipos de pokemon";
+      mesage.style.color = "white";
+      mesage.style.visibility = "visible";
+      document.getElementById('pokeType').appendChild(mesage);
+});
+
+typePokemon.addEventListener('mouseout',hiddenMesage);
+   function hiddenMesage(){
+    mesage.style.visibility = 'hidden';
+   }
