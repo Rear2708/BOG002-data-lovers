28f218272cffae6de74b20c486e8489e4e5bedd3
Ruby <alvarezruby098@gmail.com>
2021-03-23 19:28:41 -0500

    home page

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 // estas funciones son de ejemplo
 
-//
+//Una funcion que filtre los tipos de pokemon
 
 export const example = () => {
   return 'example';
--- src/main.js
+++ src/main.js
@@ -8,25 +8,27 @@ console.log(data);
 for (let i=0; i<data.pokemon.length; i++){
     console.log(data.pokemon[i].type[0]);
 };
-
-
-const typePokemon = document.getElementById('pokeType');
+const menu = document.getElementById('menu');
+const pokeTypeList = document.querySelector('ul');
+const typePokemon = document.getElementById('type1');
 const mesage = document.createElement("span");
+const newfondo = document.querySelector('body');
 
 
-typePokemon.addEventListener('click',function(){
-const newImg = document.getElementById('imagen');
-newImg.src= "../images/203876.jpg";
-});
+menu.addEventListener('click',()=>{pokeTypeList.style.visibility = "visible";});  
+
 
 typePokemon.addEventListener('mouseover',function(){
-      mesage.innerHTML = "Aqui podras consultar los tipos de pokemon";
-      mesage.style.color = "white";
+      mesage.innerHTML = "pokemon poison";
+      mesage.style.color = "black";
       mesage.style.visibility = "visible";
-      document.getElementById('pokeType').appendChild(mesage);
+      document.getElementById('type1').appendChild(mesage);
 });
 
 typePokemon.addEventListener('mouseout',hiddenMesage);
    function hiddenMesage(){
     mesage.style.visibility = 'hidden';
    }
+
+   typePokemon.addEventListener('click',()=>{newfondo.style.backgroundImage = "url(../images/531970.jpg)";
+});
