7c7d216241d8c3f3bd7d63f1d7e6333d59641648
Ruby <alvarezruby098@gmail.com>
2021-03-24 13:27:51 -0500

    pseudocodigo filtrar tipos

--- src/data.js
+++ src/data.js
@@ -1,9 +1,13 @@
-// estas funciones son de ejemplo
+
 
 //Una funcion que filtre los tipos de pokemon
 
-export const example = () => {
-  return 'example';
+export const filterData = () => {
+  for (let i=0; i<data.pokemon.length; i++){
+    console.log(data.pokemon[i].type[0]);
+}
+// agrupar por tipo especifico de pokemon y los guarde en un array
+
 };
 
 export const anotherExample = () => {
--- src/main.js
+++ src/main.js
@@ -2,17 +2,33 @@ import pokemon from './data/pokemon/pokemon.js';
 import data from './data/pokemon/pokemon.js';
 
 
-console.log(data);
-
-
-for (let i=0; i<data.pokemon.length; i++){
-    console.log(data.pokemon[i].type[0]);
-};
+//console.log(data.pokemon);
+const arrPokemon = data.pokemon;
 const menu = document.getElementById('menu');
 const pokeTypeList = document.querySelector('ul');
 const typePokemon = document.getElementById('type1');
 const mesage = document.createElement("span");
 const newfondo = document.querySelector('body');
+const tiposdePokemon = document.getElementById('bienvenido');
+let todosLosTiposJuntos = "";
+
+for (let i=0; i < arrPokemon.length; i++){
+    let tiposPokemon = arrPokemon[i].type[0];
+    todosLosTiposJuntos = todosLosTiposJuntos  + "<br>" + tiposPokemon;
+    
+};
+tiposdePokemon.innerHTML = todosLosTiposJuntos;
+
+/*crear un condicional para separar por cada tipo
+condicional para los tipo poison del la variable todosLosTiposJuntos
+convertir todosLosTiposJuntos a un array 
+luego condicionar
+si  o switch/case [i] = poison
+crear variable numeros = arrPokemon.num;
+y los guarde dentro del li con id type1
+
+*/
+
 
 
 menu.addEventListener('click',()=>{pokeTypeList.style.visibility = "visible";});  
