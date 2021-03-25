98a0922799824adc81f50d804a8732f6fdb0d6d9
Ruby <alvarezruby098@gmail.com>
2021-03-25 00:27:34 -0500

    filtrar por tipo

--- src/data.js
+++ src/data.js
@@ -2,13 +2,26 @@
 
 //Una funcion que filtre los tipos de pokemon
 
-export const filterData = () => {
-  for (let i=0; i<data.pokemon.length; i++){
-    console.log(data.pokemon[i].type[0]);
-}
-// agrupar por tipo especifico de pokemon y los guarde en un array
 
-};
+export const forType = (arrPokemon) => {
+  let tiposPokemon = [];
+  let todosLosTiposJuntos = [];
+  for (let i=0; i < arrPokemon.length; i++){
+        tiposPokemon = arrPokemon[i].type[0];
+        todosLosTiposJuntos = todosLosTiposJuntos + "\n" + tiposPokemon;
+    }
+    return todosLosTiposJuntos;
+}
+/*crear un condicional para separar por cada tipo
+condicional para los tipo poison del la variable todosLosTiposJuntos
+convertir todosLosTiposJuntos a un array 
+luego condicionar
+si  o switch/case [i] = poison
+crear variable numeros = arrPokemon.num;
+y los guarde dentro del li con id type1
+
+*/
+
 
 export const anotherExample = () => {
   return 'OMG';
--- src/main.js
+++ src/main.js
@@ -1,41 +1,23 @@
 import pokemon from './data/pokemon/pokemon.js';
 import data from './data/pokemon/pokemon.js';
+import { forType } from "./data.js";
 
 
-//console.log(data.pokemon);
 const arrPokemon = data.pokemon;
+console.log(forType(arrPokemon));
+
 const menu = document.getElementById('menu');
 const pokeTypeList = document.querySelector('ul');
 const typePokemon = document.getElementById('type1');
 const mesage = document.createElement("span");
 const newfondo = document.querySelector('body');
-const tiposdePokemon = document.getElementById('bienvenido');
-let todosLosTiposJuntos = "";
-
-for (let i=0; i < arrPokemon.length; i++){
-    let tiposPokemon = arrPokemon[i].type[0];
-    todosLosTiposJuntos = todosLosTiposJuntos  + "<br>" + tiposPokemon;
-    
-};
-tiposdePokemon.innerHTML = todosLosTiposJuntos;
-
-/*crear un condicional para separar por cada tipo
-condicional para los tipo poison del la variable todosLosTiposJuntos
-convertir todosLosTiposJuntos a un array 
-luego condicionar
-si  o switch/case [i] = poison
-crear variable numeros = arrPokemon.num;
-y los guarde dentro del li con id type1
-
-*/
-
 
 
 menu.addEventListener('click',()=>{pokeTypeList.style.visibility = "visible";});  
 
 
 typePokemon.addEventListener('mouseover',function(){
-      mesage.innerHTML = "pokemon poison";
+      mesage.innerHTML = "Buscar por tipo";
       mesage.style.color = "black";
       mesage.style.visibility = "visible";
       document.getElementById('type1').appendChild(mesage);
@@ -46,5 +28,12 @@ typePokemon.addEventListener('mouseout',hiddenMesage);
     mesage.style.visibility = 'hidden';
    }
 
-   typePokemon.addEventListener('click',()=>{newfondo.style.backgroundImage = "url(../images/531970.jpg)";
+   typePokemon.addEventListener('click',()=>{
+       document.querySelector('main').innerHTML = "";
+       newfondo.style.backgroundImage = "url(../images/531970.jpg)";
+       const containerType = document.createElement('div');
+       const lista = document.createTextNode(forType(arrPokemon));
+       containerType.appendChild(lista);
+       document.body.appendChild(containerType);
 });
+
