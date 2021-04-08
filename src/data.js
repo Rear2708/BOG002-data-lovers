e7bff0ecb54edb8b73c54c65fa936d87a35ebe40
Gimena27 <66136801+Gimena27@users.noreply.github.com>
2021-04-08 09:35:31 -0500

    menu con iconos e intento con backcard

--- src/data.js
+++ src/data.js
@@ -6,35 +6,23 @@ export const listType = (Pokemons) => {
   let todosLosTiposJuntos = [];
   for (let i=0; i < Pokemons.length; i++){
         tiposPokemon = Pokemons[i].type[0];
-        todosLosTiposJuntos.push(tiposPokemon);
-
+        if(todosLosTiposJuntos.includes(tiposPokemon)){
+          //no hacemos nada porque ya esta incluido
+        }else{
+          todosLosTiposJuntos.push(tiposPokemon);
+        }
        }
-    return [...new Set (todosLosTiposJuntos)];
+    return todosLosTiposJuntos;
 }
 export const filterType = (tipo, Pokemons) => {
   return Pokemons.filter(Pokemon => Pokemon.type[0] === tipo);
 
 }
-
-export const imageType=[
-  {name:"bug", img:"/images/pokemon-go/png/bug.png"},
-  {name:"dark",img:"/images/pokemon-go/png/dark.png"},
-  {name:"dragon",img:"/images/pokemon-go/png/dragon.png"},
-  {name:"electric",img:"/images/pokemon-go/png/electric.png"},
-  {name:"fairy",img:"/images/pokemon-go/png/fairy.png"},
-  {name:"fighting",img:"/images/pokemon-go/png/fighting.png"},
-  {name:"fire",img:"/images/pokemon-go/png/fire.png"},
-  {name:"ghost",img:"/images/pokemon-go/png/ghost.png"},
-  {name:"grass",img:"/images/pokemon-go/png/grass.png"},
-  {name:"ground",img:"/images/pokemon-go/png/ground.png"},
-  {name:"ice",img:"/images/pokemon-go/png/ice.png"},
-  {name:"normal",img:"/images/pokemon-go/png/normal.png"},
-  {name:"poison",img:"/images/pokemon-go/png/poison.png"},
-  {name:"psychic",img:"/images/pokemon-go/png/psychic.png"},
-  {name:"rock",img:"/images/pokemon-go/png/rock.png"},
-  {name:"steel",img:"/images/pokemon-go/png/steel.png"},
-  {name:"water",img:"/images/pokemon-go/png/water.png"},
-];
-export const filterIcono = (tipo, imageType) => {
-  return imageType.filter(imageType => imageType.name === tipo);
+export function upperCase(data) {
+  return data.charAt(0).toUpperCase() + data.slice(1);
 }
+
+export function ordered(data){
+  return data.sort((a,b) => a.localeCompare(b));
+}
+
--- src/main.js
+++ src/main.js
@@ -1,39 +1,43 @@
 import pokemon from './data/pokemon/pokemon.js';
 import data from './data/pokemon/pokemon.js';
-import { listType, filterType, filterIcono } from "./data.js";
+import { listType, filterType, upperCase, ordered } from "./data.js";
 
 const dataPokemon = data.pokemon;
 const selectType = listType(dataPokemon);
 const menu = document.getElementById('menu');
 const newfondo = document.querySelector('body');
-
+const select = document.querySelector('main');
+const cardPokemon = document.querySelector('#list-tipos');
 
 menu.addEventListener('click',activePokeball);
+
 
 function activePokeball() {
      const select = document.querySelector('main');
       select.innerHTML = "";
       newfondo.className="nuevoFondo";
-
-      //RECORREMOS LA LISTA Y CREAMOS UNA TARJETA POR CADA UNO
-      selectType.forEach(item => {
-      const div = document.createElement('div');
-      const p = document.createElement('p');
-      const img = document.createElement('img');
-      p.textContent = item;
-      div.id = item;
-      img.src = filterIcono;
-      document.querySelector('#list-tipos').appendChild(div).appendChild(p).appendChild(img);
-      div.classList.add('div-shadow');
-      div.addEventListener('click',mostrarPorTipo);
-     });      
-}     console.log(filterIcono)
-function mostrarPorTipo(event){
       
+      //RECORREMOS LA LISTA Y CREAMOS UNA TARJETA POR CADA UNO
+      ordered(selectType);
+      selectType.forEach(item => {
+            const div = document.createElement('div');
+            const p = document.createElement('p');
+            const img = document.createElement('img');
+            p.textContent = upperCase(item);
+            div.id = item;
+            img.src = `/images/pokemon-go/png/${item}.png`;
+            img.alt = 'pokemon';
+            document.querySelector('#list-tipos').appendChild(div).appendChild(p).appendChild(img);
+            div.classList.add('div-shadow');
+            div.addEventListener('click', frontCard); 
+
+     });      
+}     
+function frontCard(event){
       const tipo = event.currentTarget.id;
       const arrayFiltrado = filterType(tipo,dataPokemon);
-      console.log(arrayFiltrado);
-     const select = document.querySelector('main');
+      select.className = "main-hidden";
+     
      select.innerHTML = "";
      newfondo.className="nuevoFondo";
      for(let i = 0; i < arrayFiltrado.length; i++){     
@@ -46,9 +50,33 @@ function mostrarPorTipo(event){
       name.textContent = arrayFiltrado[i].name;
       num.textContent = arrayFiltrado[i].num;
       about.textContent = arrayFiltrado[i].about;
-      document.querySelector('#list-tipos').appendChild(card).appendChild(num).appendChild(name).appendChild(about);
-      document.querySelector('#list-tipos').appendChild(card).appendChild(img);
-      card.classList.add('card-pokemon');
-      console.log(name);
+      cardPokemon.appendChild(card).appendChild(num).appendChild(name).appendChild(about);
+      cardPokemon.appendChild(card).appendChild(img);
+      card.className ='card-pokemon';
+      num.className = 'numPokemon';
+      name.className = "namePokemon";
+      about.className = "aboutPokemon";
+      card.addEventListener('click', backCard);
       }
 }
+function backCard(event){
+      const tipo = event.currentTarget.id;
+      const arrayFiltrado = filterType(tipo,dataPokemon);
+      select.innerHTML = "";
+     for(let i = 0; i < arrayFiltrado.length; i++){
+      const back = document.createElement('div'); 
+      const weight= document.createElement('p');
+      const height = document.createElement('p');
+      /*const resistant = document.createElement('div');
+      const weaknesses = document.createElement('div');
+      const prevEvolution = document.createElement('div');
+      const evolution = document.createElement('div');
+      const specialAttack = document.createElement('div');*/
+      weight.textContent = arrayFiltrado[i].weight;
+      height.textContent = arrayFiltrado[i].height;
+      back.className ='card-pokemon';
+      cardPokemon.appendChild(back).appendChild(weight).appendChild(height);
+    /* cardPokemon.appendChild(backCard).appendChild(resistant).appendChild(weaknesses).appendChild(prevEvolution);
+      cardPokemon.appendChild(evolution).appendChild(specialAttack);*/
+     }
+};
